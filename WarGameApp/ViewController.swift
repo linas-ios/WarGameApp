b20320a41778116e9372659f721a88fda3355960
linas-ios <linasn404@gmail.com>
2023-05-22 18:13:16 +0200

    finish

--- WarGameApp/ViewController.swift
+++ WarGameApp/ViewController.swift
@@ -11,18 +11,29 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var leftCardImageView: UIImageView!
   @IBOutlet weak var rightCardImageView: UIImageView!
+  @IBOutlet weak var playerScore: UILabel!
+  @IBOutlet weak var cpuScore: UILabel!
+  @IBOutlet weak var playButton: UIButton!
 
-  var playerScore = 0
-  var cpuScore = 0
+  var playerScores = 0
+  var cpuScores = 0
+  var roundNumber = 0
 
 
   override func viewDidLoad() {
     super.viewDidLoad()
     leftCardImageView.image = UIImage(named: "FirstBackCard")
     rightCardImageView.image = UIImage(named: "SecondBackCard")
+
   }
 
   @IBAction func playButtonTapped(_ sender: UIButton) {
+    roundNumber += 1
+    
+    if roundNumber >= 5 {
+      playButton.isEnabled = false
+      determineWiner()
+    }
 
     guard let card1 = DeckManager.cards.randomElement(),
           let card2 = DeckManager.cards.randomElement() else {return}
@@ -34,15 +45,42 @@ class ViewController: UIViewController {
     let rank2 = DeckManager.rankOfCard(named: card2)
 
     if rank1 > rank2 {
-      playerScore += 1
-      print("Player Win round! and have \(playerScore)")
+      playerScores += 1
+      updateScore()
     } else if rank2 > rank1 {
-      cpuScore += 1
-      print("Cpu Win round! and have \(cpuScore)")
+      cpuScores += 1
+      updateScore()
+    } else if rank1 == rank2 {
+      updateScore()
     } else {
-      print("Draw")
+      print("Round Finish!")
     }
   }
 
+  func updateScore() {
+    playerScore.text = "\(playerScores)"
+    cpuScore.text = "\(cpuScores)"
+  }
+
+  func determineWiner() {
+
+    if playerScores > cpuScores {
+      showAlert(title: "Player Win!", message: "")
+    } else if cpuScores > playerScores {
+      showAlert(title: "CPU Win!", message: "")
+    } else {
+      showAlert(title: "Game is draw", message: "")
+    }
+  }
+
+}
+
+extension UIViewController {
+  func showAlert(title: String, message: String, actionTitle: String = "Ok") {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let action = UIAlertAction(title: actionTitle, style: .default , handler: nil)
+    alert.addAction(action)
+    present(alert, animated: true)
+  }
 }
 
